fd4f78ffd5ea7a9f471459fdc705bd6b30d620de
vishnu <getvishnuvs@gmail.com>
2025-09-12 11:51:42 +0530

    Addede User Query Repository

--- src/infrastructure/repositories/UserQueryRepository.ts
+++ src/infrastructure/repositories/UserQueryRepository.ts
@@ -2,48 +2,57 @@ import { UserWithOrganizerProfileDTO } from "../../domain/dtos/admin/UserWithOrg
 import { IUserQueryRepository } from "../../domain/repositories/user/IUserQueryRepository";
 import UserModel from "../db/models/UserModel";
 
-export class UserQueryRepository implements IUserQueryRepository{
+export class UserQueryRepository implements IUserQueryRepository {
   async findPendingOrganizersWithProfile(): Promise<UserWithOrganizerProfileDTO[]> {
-    try{
-    return UserModel.aggregate([
-      {$match:{role:"organizer",kycStatus:"Pending"}},
-      {
-        $lookup:{
-          from :"organizerprofiles",
-          localField:"_id",
-          foreignField:"organizerId",
-          as:"organizerProfile"
-        }
-
-      },
-      {$unwind:{path:"$organizerProfile",preserveNullAndEmptyArrays:true}},
-      {
-        $project:{
-          _id:1,
-          name: 1,
-          email: 1,
-          role: 1,
-          kycStatus: 1,
-          createdAt: 1,
-          organizerProfile: {
-            organization: "$organizerProfile.organization",
-            bio: "$organizerProfile.bio",
-            location: "$organizerProfile.location",
-            website: "$organizerProfile.website",
-            trustScore: "$organizerProfile.trustScore",
-            totalEarnings: "$organizerProfile.totalEarnings",
-            profilePicture:"$organizerProfile.profilePicture"
-          }
-
-        }
-      }
-
-    ])
-      
-  }catch(err){
-    throw new Error("Database error:failed to fetch pending organizers");
-
+    try {
+      return UserModel.aggregate([
+        {
+          $match: {
+            role: "organizer",
+            kycStatus: "Pending",
+          },
+        },
+        {
+          $lookup: {
+            from: "organizerprofiles",
+            localField: "_id",
+            foreignField: "organizerId",
+            as: "organizerProfile",
+          },
+        },
+        {
+          $unwind: {
+            path: "$organizerProfile",
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $addFields: {
+            id: { $toString: "$_id" }, 
+          },
+        },
+        {
+          $project: {
+            id: 1,
+            name: 1,
+            email: 1,
+            role: 1,
+            kycStatus: 1,
+            createdAt: 1,
+            organizerProfile: {
+              organization: "$organizerProfile.organization",
+              bio: "$organizerProfile.bio",
+              location: "$organizerProfile.location",
+              website: "$organizerProfile.website",
+              trustScore: "$organizerProfile.trustScore",
+              totalEarnings: "$organizerProfile.totalEarnings",
+              profilePicture: "$organizerProfile.profilePicture",
+            },
+          },
+        },
+      ]);
+    } catch (err) {
+      throw new Error("Database error: failed to fetch pending organizers");
+    }
   }
-}
-
 }
