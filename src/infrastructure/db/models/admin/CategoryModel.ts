ff9f50dbb05237e5f538741fceb77cbf509141c3
vishnu <getvishnuvs@gmail.com>
2025-10-14 07:46:25 +0530

    feat(category): implement Mongoose Category model with hex color validation and tag normalization

---
+++ src/infrastructure/db/models/admin/CategoryModel.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+
+export interface ICategoryDocument extends Document {
+  name: string;
+  description?: string;
+  color?:string;
+  tags: string[];
+  isBlocked: boolean;
+}
+
+const CategorySchema = new Schema<ICategoryDocument>({
+    name:{
+      type: String,
+      required:  true,
+       maxlength: 50,
+       trim: true
+    },
+    description : {
+      type: String,
+      trim: true,
+      maxlength: 200
+    },
+    color: {
+      type: String,
+    },
+    tags: {
+      type: [String],
+      default:[],
+      set: (tags: string[]) => tags.map(tag => tag.trim().toLowerCase())
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false
+    }
+},{timestamps: true})
+
+export const categoryModel = mongoose.model<ICategoryDocument>("Category", CategorySchema);
