90a51e0389e8a15f30a0fadab3eac53d3b9f8149
vishnu <getvishnuvs@gmail.com>
2025-10-31 12:36:03 +0530

    feat(admin): add routes to fetch booking details and setup dependency injection

---
+++ src/di/admin/bookings/container.ts
@@ -0,0 +1,13 @@
+import { BookingMapperAdmin } from "../../../application/mapper/admin/BookingMapperAdmin";
+import { GetAllBookingsForAdminUseCase } from "../../../application/useCases/admin/bookings/GetAllBookingsForAdminUseCase";
+import { BookingEntityFactory } from "../../../infrastructure/factories/user/BookingEntityFactory";
+import { BookingRepository } from "../../../infrastructure/repositories/user/BookingRepository";
+import { BookingControllerForAdmin } from "../../../interfaceAdapter/controllers/admin/BookingControllerForAdmin";
+
+
+const bookingEntityFactory = new BookingEntityFactory();
+const bookingRepository = new BookingRepository(bookingEntityFactory);
+const bookingMapperAdmin = new BookingMapperAdmin();
+
+const getAllBookingsForAdminUseCase = new GetAllBookingsForAdminUseCase(bookingRepository, bookingMapperAdmin);
+export const bookingControllerForAdmin = new BookingControllerForAdmin(getAllBookingsForAdminUseCase);
