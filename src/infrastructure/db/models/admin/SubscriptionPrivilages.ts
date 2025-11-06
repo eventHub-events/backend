ff1d60516acc613712fcb87020ed60a31551005e
vishnu <getvishnuvs@gmail.com>
2025-11-06 16:03:02 +0530

    feat-implements subscriptionPrivilegesSchema

---
+++ src/infrastructure/db/models/admin/SubscriptionPrivilages.ts
@@ -0,0 +1,48 @@
+import { Schema } from "mongoose";
+
+export interface Privileges {
+  maxActiveEvents : number;
+  maxFeaturedEvents: number;
+  payout: {
+    frequency: "within-1-week" | "within-2-weeks" | "within-1-month";
+    delayDays: number;
+  };
+  supportLevel : "email"|"priority";
+  commissionRate : number;
+
+
+}
+
+export const privilegesSchema =  new Schema<Privileges>({
+    maxActiveEvents  : {
+      type : Number,
+      required: true
+     },
+     maxFeaturedEvents : {
+        type: Number,
+        required: true
+     },
+     payout : {
+        frequency : {
+           type : String,
+           enum :["within-1-week", "within-2-weeks","within-1-week"],
+           required: true
+         },
+       delayDays : {
+         type : Number,
+         required: true
+        }
+      },
+      supportLevel : {
+              type :String,
+              enum: ["email" , "priority"]
+      },
+     commissionRate : {
+        type: Number
+     },
+
+  
+
+
+},{_id: false, timestamps: true},)
+
