32d67ce4e084ada85927dec11971e582b40e7304
vishnu <getvishnuvs@gmail.com>
2025-11-09 12:54:47 +0530

    feat: add the OrganizerSubscriptionRepository

--- src/domain/repositories/organizer/IOrganizerSubscriptionRepository.ts
+++ src/domain/repositories/organizer/IOrganizerSubscriptionRepository.ts
@@ -1,8 +1,9 @@
 import { OrganizerSubscriptionEntity } from "../../entities/organizer/OrganizerSubscriptionEntity";
+import { SubscriptionStatus } from "../../enums/organizer/subscription";
 
 export interface IOrganizerSubscriptionRepository {
   createSubscription(entity: OrganizerSubscriptionEntity) : Promise<OrganizerSubscriptionEntity>;
-  updateSubscription(entity: OrganizerSubscriptionEntity): Promise<OrganizerSubscriptionEntity>;
-  fetchSubscriptionById(entity: OrganizerSubscriptionEntity) : Promise<OrganizerSubscriptionEntity>;
+  updateSubscription(subscriptionId: string, entity: OrganizerSubscriptionEntity): Promise<OrganizerSubscriptionEntity>;
+  fetchSubscriptionById(organizerId: string,status: SubscriptionStatus) : Promise<OrganizerSubscriptionEntity>;
 
 }
--- src/infrastructure/repositories/organizer/OrganizerSubscriptionRepository.ts
+++ src/infrastructure/repositories/organizer/OrganizerSubscriptionRepository.ts
@@ -1,20 +1,31 @@
+import { IOrganizerSubscriptionEntityFactory } from "../../../application/interface/factories/organizer/IOrganzierSubscriptonEntityFactory";
 import { OrganizerSubscriptionEntity } from "../../../domain/entities/organizer/OrganizerSubscriptionEntity";
+import { SubscriptionStatus } from "../../../domain/enums/organizer/subscription";
 import { IOrganizerSubscriptionRepository } from "../../../domain/repositories/organizer/IOrganizerSubscriptionRepository";
+import { OrganizerSubscriptionDbModel } from "../../../domain/types/OrganizerTypes";
 import { IOrganizerSubscription, OrganizerSubscriptionModel } from "../../db/models/organizer/subscription/OrganizerSubcriptionModel";
 import { BaseRepository } from "../BaseRepository";
 
 export class OrganizerSubscriptionRepository extends BaseRepository<IOrganizerSubscription> implements IOrganizerSubscriptionRepository {
 
-   constructor(){
+   constructor(
+         private _SubscriptionEntityFactory : IOrganizerSubscriptionEntityFactory
+   ){
      super(OrganizerSubscriptionModel)
    }
-  createSubscription(entity: OrganizerSubscriptionEntity): Promise<OrganizerSubscriptionEntity> {
-      
+ async createSubscription(entity: OrganizerSubscriptionEntity): Promise<OrganizerSubscriptionEntity> {
+      const created =  await super.create(entity) as OrganizerSubscriptionDbModel;
+  return this._SubscriptionEntityFactory.toDomain(created);
   }
-  updateSubscription(entity: OrganizerSubscriptionEntity): Promise<OrganizerSubscriptionEntity> {
-      
+ async  updateSubscription(subscriptionId: string, entity: OrganizerSubscriptionEntity): Promise<OrganizerSubscriptionEntity> {
+       const updated = await super.update(subscriptionId, entity) as OrganizerSubscriptionDbModel;
+    return this._SubscriptionEntityFactory.toDomain(updated);
   }
-  fetchSubscriptionById(entity: OrganizerSubscriptionEntity): Promise<OrganizerSubscriptionEntity> {
-      
+  
+  async fetchSubscriptionById(organizerId: string, status: SubscriptionStatus): Promise<OrganizerSubscriptionEntity> {
+
+      const fetched = await super.findOne({organizerId, status}) as OrganizerSubscriptionDbModel;
+  return this._SubscriptionEntityFactory.toDomain(fetched);
+
   }
 }
